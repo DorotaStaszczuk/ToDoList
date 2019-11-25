d51f92cbc853b92eb42ef428188ec3d08d10e41a
Dorota Staszczuk <dorota.staszczuk@gmail.com>
2019-11-25 15:46:30 +0100

    js file added and adding tasks is working

---
+++ app.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function(){
+	const selected = [];
+	const allTasks = [];
+
+	const removeAllBtn = document.getElementById('removeAll');
+	const removeSelectBtn = document.getElementById('removeSelect');
+	const taskMsg = document.querySelector("#taskMsg");
+	const addTaskBtn = document.querySelector("#addTask");
+	const mainTable = document.getElementById('mainTable');
+	let count = 0
+	
+	const add = ()=> {
+		count++
+	const countCell = document.createElement("td");
+	const msgCell = document.createElement("td");
+	const taskMsg = document.querySelector("#taskMsg").value;
+	const newRow = document.createElement('tr');
+	msgCell.innerText = taskMsg;
+	countCell.innerText = count;
+	newRow.appendChild(countCell);
+	newRow.appendChild(msgCell);
+
+		mainTable.appendChild(newRow);
+		console.log(taskMsg);
+
+	};
+	addTaskBtn.addEventListener('click', add);
+	
+
+	const select = () => {
+
+	};
+	const remove = () => {
+
+	};
+	const removeAll = () => {
+
+	};
+	const removeSelected = () => {
+
+	};
+
+})
